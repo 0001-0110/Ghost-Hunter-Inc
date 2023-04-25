24778b68ad1271a4cd8f13a2861a689e72f65eca
Rémi TROTEL <remi.trotel@epita.fr>
2023-04-25 23:59:49 +0200

    Reworked player movements and added sprite selection

--- Ghost-Hunter/Assets/Scripts/PlayerMovement.cs
+++ Ghost-Hunter/Assets/Scripts/PlayerMovement.cs
@@ -1,160 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     private Transform myTransform;
     private Rigidbody2D myRigidbody;
-    private Vector2 movement;
-    
+    private SpriteRenderer mySpriteRenderer;
 
     public float speed = 2f;
-    
-    // Start is called before the first frame update
+
     void Start()
     {
         myTransform = GetComponent<Transform>();
         myRigidbody = GetComponent<Rigidbody2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-        
-        bool horizontalButtonPressed = Input.GetButton("Horizontal");
-        bool verticalButtonPressed = Input.GetButton("Vertical");
-
-        //code inspired by a Brackeys video
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        myRigidbody.velocity = movement.normalized * speed;
-        rotateCharacter(movement.normalized);
-        /*
-        if (horizontalButtonPressed || verticalButtonPressed)
-        {
-            moveCharacter(horizontalButtonPressed,verticalButtonPressed);
-           
-        }
-        else
-        {
-            myRigidbody.velocity = Vector2.zero;
-        }
-
-        */
+        // Code inspired by maths ;)
+        SetVelocity(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        SetSprite();
     }
 
-    void moveCharacter(bool moveHorizontal, bool moveVertical)
+    private void SetVelocity(Vector2 velocity)
     {
-       
-        float direction;
-        float rotation;// = 0f;
-
-        if (moveHorizontal && !moveVertical)
-        {
-
-            direction = Input.GetAxis("Horizontal");
-
-            myRigidbody.velocity = Vector2.right * (direction * speed);
-
-            if (direction < 0)//a pressed down
-            {
-                rotation = 90f;
-            }
-            else //d pressed down
-            {
-                rotation = 270f;
-            }
-        }
-
-        else if (moveVertical && !moveHorizontal)
-        {
-
-            direction = Input.GetAxis("Vertical");
-
-            myRigidbody.velocity = Vector2.up * (direction * speed);
-            
-            if (direction < 0)//s pressed down
-            {
-                rotation = 180f;
-            }
-            else //w pressed down
-            {
-                rotation = 0f;
-            }
-        }
-        else //if two buttons are pressed at the same time
-        {
-            return;
-        }
-        
-        Vector3 newRotation = new Vector3(0, 0,rotation);
-        myTransform.rotation = Quaternion.Euler(newRotation);
+        // The speed must be normalized to avoid diagonal movements from being faster than straight ones
+        myRigidbody.velocity = velocity.normalized * speed;
     }
 
-    private void rotateCharacter(Vector2 movement)
+    private void SetSprite()
     {
-
-        if (movement == Vector2.zero)
+        // Get the angle of the player based on his velocity
+        // Atan2 gives the result in radians, so we need to convert it to degrees
+        float angle = Mathf.Rad2Deg * Mathf.Atan2(myRigidbody.velocity.y, myRigidbody.velocity.x);
+        // Pick the correct sprite based on the angle
+        // TODO Replace nulls with sprites
+        mySpriteRenderer.sprite = angle switch
         {
-            return;
-        }
-        float xDirection = movement.x;
-        float yDirection = movement.y;
-
-        float myRotation = 0;
-
-        if (xDirection > 0)//if x is positive, we want to start at 0 degrees
-        {
-            myRotation = 270;
-
-            if (yDirection > 0)//if y is also positive, we want to go up a little
-            {
-                myRotation = 315;
-            }
-            else if (yDirection < 0)//if y is negative, we want to go down to 315 degrees
-            {
-                myRotation = 225;
-            }
-
-
-        }
-        else if (xDirection < 0)//if x is negative, we want to go leftish, starting at 180 degrees
-        {
-            myRotation = 90;
-
-            if (yDirection > 0)//if y is positive, we want to go left and up
-            {
-                myRotation = 45;
-            }
-            else if (yDirection < 0)
-            {
-                myRotation = 135;
-            }
-
-        }
-        else //if x is 0, we want to only use the y value
-        {
-            if (yDirection > 0)
-            {
-                myRotation = 0;
-            }
-            else
-            {
-                myRotation = 180;
-            }
-        }
-        
-
-        // Debug.Log($"Rotation should be {myRotation}");
-        
-        //TODO figure out how to set the rotation to the value I want
-        Vector3 newRotation = new Vector3(0, 0, myRotation);
-        myTransform.rotation = Quaternion.Euler(newRotation);
-        //myTransform.Rotate(Vector3.zero);
-        //myTransform.Rotate(new Vector3(0,0,myRotation));
+            // Going to the left
+            float x when x >= -135 && x < -45 => null,
+            // Going upward
+            float x when x >= -45 && x < 45 => null,
+            // Going to the right
+            float x when x >= 45 && x < 135 => null,
+            // Going downward
+            _ => null,
+        };
     }
-    
 }
